21653cb88b36e3839051f8757f29347ee1079b75
jonathan <jonathan.smith@inbotiqa.com>
2022-03-28 12:42:34 +0100

    refactor of files

---
+++ python-easy-steps/01-operations/comparison.py
@@ -0,0 +1,13 @@
+nil = 0
+num = 0
+maximum = 1
+cap = 'A'
+low = 'a'
+
+print('Equality :\t\t', nil, '==', num, '\t', nil == num)
+print('Equality :\t\t', cap, '==', low, '\t', cap == low)
+print('Inequality :\t', nil, '!=', maximum, '\t', nil != maximum)
+print('Greater :\t\t', nil, '>', maximum, '\t\t', nil > maximum)
+print('Lesser: \t\t', nil, '<', maximum, '\t\t', nil < maximum)
+print('More or Equal: \t', nil, '>=', num, '\t', nil >= num)
+print('Less or Equal: \t', maximum, '<=', num, '\t', maximum <= num)
