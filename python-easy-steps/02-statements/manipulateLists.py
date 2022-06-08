5ecf4963451874296db005f5e95cfa4dd46fb9b6
jonathan <jonathan.smith@inbotiqa.com>
2022-06-08 10:03:13 +0100

    update to 02-statements

--- python-easy-steps/02-statements/manipulateLists.py
+++ python-easy-steps/02-statements/manipulateLists.py
@@ -1,24 +1,74 @@
 basket = ['Apple', 'Bun', 'Cola']
 crate = ['Egg', 'Fig', 'Grape']
 
+
 print('\nBasket List:\t\t', basket)
 print('No of Basket items:\t', len(basket))
-print('------------------------------------')
+print('\nCrate List:\t\t\t', crate)
+print('No of crates items:\t', len(crate))
+print('\nTotal number of items:', len(basket) + len(crate))
+print('\n------------------------------------')
 
+# Appends an item to the end of the list
+print('\nAppend an item to the list:')
 basket.append('Damson')
 basket_count = len(basket)
-print('\nAdded:\t\t\t\t', basket[basket_count - 1])
-print('Basket List:\t\t', basket)
+print('Added:\t\t\t\t', basket[basket_count - 1])
+print('New Basket List:\t', basket)
+print('\n------------------------------------')
 
-print('\nLast item removed:\t', basket.pop())
-print('Basket List:\t\t', basket)
-print('No of Basket items:\t', len(basket))
-print('------------------------------------')
-
+# Adds all items to the end of the list
+print('\nExtend the list:')
 basket.extend(crate)
-print('\nCrate list added:\t', crate)
-print('Extended basket:\t', basket)
+print('Crate list to be added:\t', crate)
+print('New Extended basket:\t', basket)
+print('\n------------------------------------')
 
+# Inserts item at an indexed position
+print('\nInsert an item at pos: 2')
+basket.insert(2, 'Strawberries')
+basket_count = len(basket)
+print('Added:\t\t\t\t', basket[basket_count - 6])
+print('New Basket List:\t', basket)
+print('\n------------------------------------')
+
+# Remove last item from the list
+print('\nRemove the last item from the basket')
+print('Last item removed:\t', basket.pop())
+print('Basket List:\t\t', basket)
+print('\n------------------------------------')
+
+# Remove an item form the list using an index
+print('\nRemove an item at pos: 2')
+print('Specified item removed:\t', basket.pop(2))
+print('Basket List:\t\t\t', basket)
+print('\n------------------------------------')
+
+# Returns index position of specified item
+print('\nReturns a specified item by string')
+print('Specified item located at pos\t', basket.index('Cola')+1)
+print('Basket List:\t\t\t\t\t', basket)
+print('\n------------------------------------')
+
+# Return the amount of times one specified item is in the list
+print('\nReturns a count of a specified item')
+print('How many times does BUN appear', basket.count('Bun'))
+print('\n------------------------------------')
+
+# Sorts all list items
+print('\nSorts the list')
+basket.sort()
+print(basket)
+print('\n------------------------------------')
+
+# Reverse all list items
+print('\nReverses the list')
+basket.reverse()
+print(basket)
+print('\n------------------------------------')
+
+# Reset the program
+print('\nReset the list')
 del basket[3::]
-print('\nCrate list removed:\t', crate)
+print('Crate list removed:\t', crate)
 print('Basket List:\t\t', basket)
--- python-easy-steps/02-statements/restrictingLists.py
+++ python-easy-steps/02-statements/restrictingLists.py
@@ -1,17 +1,57 @@
+print('\nInitial tuple + set:')
+print('-----------------------------------')
+
 # A user-defined tuple
+# Immutable - tuple(s)
 zoo = ('Kangaroo', 'Leopard', 'Moose')
 print('Tuple:', zoo, '\tLength:', len(zoo))
 print(type(zoo))
 
 # A user-defined set
+# Restricted to unique values
 bag = {'Red', 'Green', 'Blue'}
-bag.add('Yellow')
-print('\nSet:', bag, '\tLength:', len(bag))
+print('\nSet:', bag, '\t\t\t\tLength:', len(bag))
 print(type(bag))
+print('-----------------------------------')
 
+print('\nSet methods:')
+print('-----------------------------------')
+
+# Adds item x to the set
+print('Add item x to the set')
+# code
+print('-----------------------------------')
+
+# Adds multiple items to the set
+print('Adds multiple items to the set')
+# code
+print('-----------------------------------')
+
+# Returns a copy of the set
+print('Returns a copy of the set')
+# code
+print('-----------------------------------')
+
+# Removes a random item from the set
+print('Removes a random item from the set')
+# code
+print('-----------------------------------')
+
+# Removes item x if found in the set
+print('Removes item x if found in the set')
+# code
+print('-----------------------------------')
+
+# Returns items that appear in both sets
+print('Removes item x if found in the set')
+# code
+print('-----------------------------------')
+
+# Check if item exists in the set
 print('\nIs Green in bag Set?:', 'Green' in bag)
 print('Is Orange in bag Set?:', 'Orange' in bag)
 
+# Check if item exists in the tuple
 print('\nIs Cat in zoo tuple?:', 'Cat' in zoo)
 print('Is Moose in zoo tuple?:', 'Moose' in zoo)
 
